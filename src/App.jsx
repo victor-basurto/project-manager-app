44e515384ff20abf34a7a83fb30cecb91441a681
Victor Basurto <my.mvplace@gmail.com>
2025-09-24 21:53:20 -0700

    feat: handle delete project

--- src/App.jsx
+++ src/App.jsx
@@ -45,10 +45,23 @@ function App() {
       };
     });
   }
+  function handleDeleteProject() {
+    setProjectState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+        projects: prevState.projects.filter(
+          (project) => project.id !== prevState.selectedProjectId,
+        ),
+      };
+    });
+  }
   const selectedProject = projectState.projects.find(
     (project) => project.id === projectState.selectedProjectId,
   );
-  let content = <SelectedProject project={selectedProject} />;
+  let content = (
+    <SelectedProject project={selectedProject} onDelete={handleDeleteProject} />
+  );
   if (projectState.selectedProjectId === null) {
     content = (
       <NewProject onAdd={handleAddProject} onCancel={handleCancelAddProject} />
